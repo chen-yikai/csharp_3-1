e681317401b3a2804a553342d52c89b57e4ceb60
eliaschen <eliaschen.dev@gmail.com>
2024-12-04 11:39:39 +0800

    add box

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,16 +29,21 @@
         private void InitializeComponent()
         {
             this.countNum = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.myNumPluser = new System.Windows.Forms.Label();
+            this.canDiv = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.label3 = new System.Windows.Forms.Label();
+            this.increase1 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.label4 = new System.Windows.Forms.Label();
+            this.increase2 = new System.Windows.Forms.Label();
             this.button4 = new System.Windows.Forms.Button();
-            this.label5 = new System.Windows.Forms.Label();
+            this.increase3 = new System.Windows.Forms.Label();
             this.button5 = new System.Windows.Forms.Button();
+            this.bornYear = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.box = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // countNum
@@ -51,26 +56,27 @@
             this.countNum.TabIndex = 0;
             this.countNum.Text = "座號~71的總和";
             this.countNum.UseVisualStyleBackColor = true;
+            this.countNum.Click += new System.EventHandler(this.countNum_Click);
             // 
-            // label1
+            // myNumPluser
             // 
-            this.label1.AutoSize = true;
-            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.label1.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label1.Location = new System.Drawing.Point(210, 19);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(2, 23);
-            this.label1.TabIndex = 1;
+            this.myNumPluser.AutoSize = true;
+            this.myNumPluser.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.myNumPluser.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.myNumPluser.Location = new System.Drawing.Point(210, 19);
+            this.myNumPluser.Name = "myNumPluser";
+            this.myNumPluser.Size = new System.Drawing.Size(2, 23);
+            this.myNumPluser.TabIndex = 1;
             // 
-            // label2
+            // canDiv
             // 
-            this.label2.AutoSize = true;
-            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.label2.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label2.Location = new System.Drawing.Point(12, 126);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(2, 23);
-            this.label2.TabIndex = 3;
+            this.canDiv.AutoSize = true;
+            this.canDiv.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.canDiv.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.canDiv.Location = new System.Drawing.Point(12, 118);
+            this.canDiv.Name = "canDiv";
+            this.canDiv.Size = new System.Drawing.Size(2, 23);
+            this.canDiv.TabIndex = 3;
             // 
             // button1
             // 
@@ -82,16 +88,17 @@
             this.button1.TabIndex = 2;
             this.button1.Text = "4~207可以被7整除的數字";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // label3
+            // increase1
             // 
-            this.label3.AutoSize = true;
-            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.label3.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label3.Location = new System.Drawing.Point(210, 170);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(2, 23);
-            this.label3.TabIndex = 5;
+            this.increase1.AutoSize = true;
+            this.increase1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.increase1.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.increase1.Location = new System.Drawing.Point(210, 170);
+            this.increase1.Name = "increase1";
+            this.increase1.Size = new System.Drawing.Size(2, 23);
+            this.increase1.TabIndex = 5;
             // 
             // button2
             // 
@@ -103,27 +110,30 @@
             this.button2.TabIndex = 4;
             this.button2.Text = "5x5個星號";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
             this.button3.AutoSize = true;
             this.button3.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.button3.Location = new System.Drawing.Point(12, 236);
+            this.button3.Location = new System.Drawing.Point(24, 313);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(167, 35);
             this.button3.TabIndex = 6;
             this.button3.Text = "顯示九九乘法";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
-            // label4
+            // increase2
             // 
-            this.label4.AutoSize = true;
-            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.label4.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label4.Location = new System.Drawing.Point(505, 170);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(2, 23);
-            this.label4.TabIndex = 8;
+            this.increase2.AutoSize = true;
+            this.increase2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.increase2.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.increase2.Location = new System.Drawing.Point(505, 170);
+            this.increase2.Name = "increase2";
+            this.increase2.Size = new System.Drawing.Size(2, 23);
+            this.increase2.TabIndex = 8;
+            this.increase2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // button4
             // 
@@ -135,16 +145,18 @@
             this.button4.TabIndex = 7;
             this.button4.Text = "遞增顯示星號";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
-            // label5
+            // increase3
             // 
-            this.label5.AutoSize = true;
-            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.label5.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label5.Location = new System.Drawing.Point(797, 170);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(2, 23);
-            this.label5.TabIndex = 10;
+            this.increase3.AutoSize = true;
+            this.increase3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.increase3.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.increase3.Location = new System.Drawing.Point(797, 170);
+            this.increase3.Name = "increase3";
+            this.increase3.Size = new System.Drawing.Size(2, 23);
+            this.increase3.TabIndex = 10;
+            this.increase3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // button5
             // 
@@ -156,22 +168,76 @@
             this.button5.TabIndex = 9;
             this.button5.Text = "遞減顯示星號";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // bornYear
+            // 
+            this.bornYear.Location = new System.Drawing.Point(648, 659);
+            this.bornYear.Name = "bornYear";
+            this.bornYear.Size = new System.Drawing.Size(89, 22);
+            this.bornYear.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label6.Location = new System.Drawing.Point(419, 659);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(223, 21);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "輸入出生民國年(1-112):";
+            // 
+            // button6
+            // 
+            this.button6.AutoSize = true;
+            this.button6.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button6.Location = new System.Drawing.Point(759, 652);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(167, 35);
+            this.button6.TabIndex = 14;
+            this.button6.Text = "年齡及生肖查詢";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.AutoSize = true;
+            this.button7.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button7.Location = new System.Drawing.Point(24, 645);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(167, 35);
+            this.button7.TabIndex = 15;
+            this.button7.Text = "進階誠實問答";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // box
+            // 
+            this.box.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.box.Location = new System.Drawing.Point(24, 354);
+            this.box.Multiline = true;
+            this.box.Name = "box";
+            this.box.Size = new System.Drawing.Size(883, 233);
+            this.box.TabIndex = 16;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(904, 450);
-            this.Controls.Add(this.label5);
+            this.ClientSize = new System.Drawing.Size(938, 699);
+            this.Controls.Add(this.box);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.bornYear);
+            this.Controls.Add(this.increase3);
             this.Controls.Add(this.button5);
-            this.Controls.Add(this.label4);
+            this.Controls.Add(this.increase2);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
-            this.Controls.Add(this.label3);
+            this.Controls.Add(this.increase1);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.canDiv);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.myNumPluser);
             this.Controls.Add(this.countNum);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -183,16 +249,21 @@
         #endregion
 
         private System.Windows.Forms.Button countNum;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label myNumPluser;
+        private System.Windows.Forms.Label canDiv;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label increase1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label increase2;
         private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label increase3;
         private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox bornYear;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.TextBox box;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,78 @@ namespace _3_1
         {
             InitializeComponent();
         }
+
+        private void countNum_Click(object sender, EventArgs e)
+        {
+            int myNumber = 27;
+            int resNum=0;
+            for(int i = myNumber; i <= 71;i++)
+            {
+                resNum += i;
+            }
+            myNumPluser.Text = resNum.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string resNums = "";
+            for(int i = 4; i <= 207; i++)
+            {
+                if(i % 7 == 0)
+                {
+                    resNums += i.ToString()+" ";
+                }
+            }
+            canDiv.Text = resNums;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    increase1.Text += "*";
+                }
+                increase1.Text += "\r\n";
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    increase2.Text += "*";
+                }
+                increase2.Text += "\r\n";
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            for(int i = 5; i >= 1; i--)
+            {
+                for( int j = 0; j < i; j++)
+                {
+                    increase3.Text += "*";
+                }
+                increase3.Text += "\r\n";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            for(int i = 1; i <= 9; i++)
+            {
+                for(int j = 1; j <= 9; j++)
+                {
+                    box.Text += $"{j}*{i}={i*j}\t";
+                }
+                box.Text += "\r\n";
+            }
+        }
     }
 }
